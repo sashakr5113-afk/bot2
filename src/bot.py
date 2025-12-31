ecb48b439432753992336c4f7ecd1d00b6f75a72
sashakr5113-afk <sashakr5113@gmail.com>
2025-12-31 15:44:07 +0300

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -1,9 +1,12 @@
-import os
 import discord
+import os
 
-TOKEN = os.getenv('DISCORD_TOKEN')
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} онлайн!')
+    print(f'{client.user} онлайн!')  # Обязательно print!
+
+client.run(os.getenv('DISCORD_TOKEN'))
